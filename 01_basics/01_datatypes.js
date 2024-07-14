3f2bc526c791c346fd73a3481260027b84218f34
Sachin Kumar Yadav <160302367+sachinkumar5086@users.noreply.github.com>
2024-07-14 13:49:47 +0000

    completed

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,5 @@
+"use strict"; // treat all js code as newer version
+console.log(typeof "Sachin"); //string
+console.log(typeof 45);  //number
+console.log(typeof null); //object 
+console.log(typeof undefined); //undefined
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 13424
+let accountEmail = "sachin@gmail.com"
+accountCity = "Bangluru"
+let age
+/*
+console.log(accountEmail)
+*/
+console.table([accountId,accountEmail,accountCity,age])
